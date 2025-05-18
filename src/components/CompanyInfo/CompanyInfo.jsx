1cfddaf56453349b3317caf4d52511fdb51126dd
Rin <ndoraguniru25@gmail.com>
2025-05-18 17:40:14 +0900

    webサイト修正1

--- src/components/CompanyInfo/CompanyInfo.jsx
+++ src/components/CompanyInfo/CompanyInfo.jsx
@@ -10,11 +10,13 @@ const CompanyInfo = forwardRef((props, ref) => {
           <table className="company-info-table">
             <tbody>
               <tr><th>会社名</th><td>株式会社ミナカノ</td></tr>
-              <tr><th>設立</th><td>2024年1月1日</td></tr>
-              <tr><th>所在地</th><td>福井県福井市大手3丁目</td></tr>
+              <tr><th>設立</th><td>2025年5月8日</td></tr>
+              <tr><th>所在地</th><td>福井県鯖江市旭町1丁目8-4</td></tr>
               <tr><th>代表者</th><td>田濃 一翔</td></tr>
-              <tr><th>資本金</th><td>300万円</td></tr>
-              <tr><th>事業内容</th><td>教育サービス、AIプロダクト開発</td></tr>
+              <tr><th>資本金</th><td>100万円</td></tr>
+              <tr><th>事業内容</th><td>1. 教育ITサービスの開発、提供及び運営
+                2. 教育関連アプリの企画、開発、販売及びサポート業務
+                3. 教育コンテンツの制作及び配信</td></tr>
             </tbody>
           </table>
         </div>
